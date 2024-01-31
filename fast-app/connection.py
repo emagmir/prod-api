9738cb39e4abff6f871c334707f48e41790f63cd
emagmir <xmagureanmircea@gmail.com>
2024-01-31 17:24:27 +0200

    lets get going

--- fast-app/connection.py
+++ fast-app/connection.py
@@ -1,9 +1,9 @@
 from pymongo import MongoClient
 
-db_addr = "mongodb-service"
+db_addr = "mongodb://pythonadmin:pythonadmin@db.fastmongo.com:27017/?replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
 db_ip = 27017
 
 
-client = MongoClient(db_addr, db_ip)
+client = MongoClient(db_addr)
 db = client.users
 collection = db.myusers
